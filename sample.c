ff696ef1010269c18e9c7c9dd0d727a0deaaebe7
mammukb <aarongladston2003@gmail.com>
2024-11-28 15:35:55 +0530

    pgms changed

--- sample.c
+++ sample.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-int check(int x);
-int create(int n);
+int check(int x,struct bitString *head);
 
 struct bitString
 {
@@ -14,13 +13,11 @@ int main()
     int a[10] = {0};
     int n1,n2, x;
     int u[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    printf("Universal set \nU=:{1,2,3,4,5,6,7,8,9,10}\n");
+    printf("Universal set \n U=:{1,2,3,4,5,6,7,8,9,10}\n");
     while(1){
     printf("Enter the size of set 1:");
     scanf("%d", &n1);
-    printf("Enter the size of set 2:");
-    scanf("%d", &n2);
-    if(n1>10 && n2>10)
+    if(n1>10)
     {
         printf("enter a valid size\n");
     } 
@@ -28,55 +25,18 @@ int main()
         break;
     }
     }
-    create(n1,head1);
-    create(n2,head2);
-    printf("The first set is:");
-    print(head1);
-    printf("The second set is:");
-    print(head2);
-
-   
-   
-    // for (int i =0 ; i<10 ;i++) {
-    // for(temp=head;temp!=NULL;temp=temp->next){
-    //     if (u[i] == temp->data) 
-    //        a[i] = 1;
-    // }
-        
-    // }
-    // printf("\nBit String=");
-    // for(int i=0;i<10;i++){
-    //     printf("%d",a[i]);
-    // }
-}
-int check(int x){
- if (head==NULL)
- {
-    return 1;
- }
- else{
-     for(temp=head;temp!=NULL;temp=temp->next){
-        if(x==temp->data){
-            return 0;
-        }
-     }
-     return 1;
- }
-}
-
-int create(int n,struct bitString *head){
-     printf("Enter your Elemnets from universal set : \n");
-     for (int i = 1; i <= n; i++)
+    printf("Enter your Elemnets from universal set : \n");
+    for (int i = 1; i <= n1; i++)
     {
         printf("Enter %d th element:", i);
         scanf("%d",&x);
         if(x>=1 && x<=10){
-            if(check(x)){
+            if(check(x,head1)){
                 newnode = (struct bitString* )malloc(sizeof(struct bitString));
                 newnode->data =x;
                 newnode->next = NULL;
-                if(head == NULL) {
-                    head = newnode;
+                if(head1 == NULL) {
+                    head1 = newnode;
                     current = newnode;
                 }
                 else {
@@ -95,10 +55,89 @@ int create(int n,struct bitString *head){
             i=i-1;
         }
     }
-}
-
-void print(struct bitString *head){
- for(temp=head;temp!=NULL;temp=temp->next){
+    for(temp=head1;temp!=NULL;temp=temp->next){
         printf("%d\t",temp->data);
     }
+    for (int i =0 ; i<10 ;i++) {
+    for(temp=head1;temp!=NULL;temp=temp->next){
+        if (u[i] == temp->data) 
+           a[i] = 1;
+    }
+        
+    }
+    printf("\nBit String=");
+    for(int i=0;i<10;i++){
+              printf("%d",a[i]);
+    }
+
+    while(1){
+    printf("Enter the size of set 2:");
+    scanf("%d", &n2);
+    if(n2>10)
+    {
+        printf("enter a valid size\n");
+    } 
+    else{
+        break;
+    }}
+    
+    printf("Enter your Elemnets from universal set : \n");
+    for (int i = 1; i <= n1; i++)
+    {
+        printf("Enter %d th element:", i);
+        scanf("%d",&x);
+        if(x>=1 && x<=10){
+            if(check(x,head2)){
+                newnode = (struct bitString* )malloc(sizeof(struct bitString));
+                newnode->data =x;
+                newnode->next = NULL;
+                if(head2 == NULL) {
+                    head2 = newnode;
+                    current = newnode;
+                }
+                else {
+                    current->next = newnode;
+                    current = newnode;
+                }
+            }
+            else{
+                printf("Element already in the set\n");
+                i=i-1;
+            }
+       
+        }
+        else{
+            printf("choose your elements from U \n");
+            i=i-1;
+        }
+    }
+    for(temp=head2;temp!=NULL;temp=temp->next){
+        printf("%d\t",temp->data);
+    }
+    for (int i =0 ; i<10 ;i++) {
+    for(temp=head2;temp!=NULL;temp=temp->next){
+        if (u[i] == temp->data) 
+           a[i] = 1;
+    }
+        
+    }
+    printf("\nBit String=");
+    for(int i=0;i<10;i++){
+              printf("%d",a[i]);
+    }
+    }
+
+int check(int x,struct bitString *head){
+ if (head==NULL)
+ {
+    return 1;
+ }
+ else{
+     for(temp=head;temp!=NULL;temp=temp->next){
+        if(x==temp->data){
+            return 0;
+        }
+     }
+     return 1;
+ }
 }
