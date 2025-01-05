b6e8b335712c448e0a996951b9ef52729fed99d5
mammukb <muhammedkb156@gmail.com>
2025-01-05 22:58:52 +0530

    kruskals completed

--- kruskals.c
+++ kruskals.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
-
 #define MAX 10
 int parent[MAX];
-
 int find(int i)
 {
     while (parent[i] != i)
         i = parent[i];
     return i;
 }
-
 void uni(int i, int j)
 {
     if (i != j)
@@ -17,14 +14,12 @@ void uni(int i, int j)
         parent[j] = i;
     }
 }
-
 int main()
 {
     int num_vertex, i, v1, v2, mstcost = 0;
     printf("Enter the Number of Vertex :");
     scanf("%d", &num_vertex);
-    int vertices[num_vertex], cost[num_vertex][num_vertex], edge1[num_vertex], edge2[num_vertex];
-    // printf("Enter the Vertices :\n");
+    int vertices[num_vertex], cost[num_vertex][num_vertex], mst[num_vertex][num_vertex];
     for (int i = 0; i < num_vertex; i++)
     {
         parent[i] = i;
@@ -40,36 +35,53 @@ int main()
             {
                 cost[i][j] = 999;
             }
+            mst[i][j] = 0;
         }
     }
-
-    for (int i = 0, min = 999, e = 0; i < num_vertex; i++)
+    int e = 0;
+    int min = 999;
+    while (e < num_vertex - 1)
     {
-        for (int j = 0; j < num_vertex; j++)
+        for (int i = 0; i < num_vertex; i++)
         {
-            if (cost[i][j] < min)
+            for (int j = 0; j < num_vertex; j++)
             {
-                min = cost[i][j];
-                v1 = i;
-                v2 = j;
+                if (cost[i][j] < min)
+                {
+                    min = cost[i][j];
+                    v1 = i;
+                    v2 = j;
+                }
             }
         }
         if (find(v1) != find(v2))
         {
-            edge1[e] = v1;
-            edge2[e] = v2;
             e++;
             mstcost += min;
-
+            mst[v1][v2] = mst[v2][v1] = min;
+            cost[v1][v2] = cost[v2][v1] = 999;
             uni(v1, v2);
+            min = 999;
         }
-
-        min = 999;
     }
-
-    printf("The MST contains Edges");
+    printf("MST Contains:\n");
     for (int i = 0; i < num_vertex; i++)
     {
-        printf("%d -- %d\n", edge1[i], edge2[i]);
+        for (int j = i; j < num_vertex; j++)
+        {
+            if(mst[i][j]!=0){
+            printf("%d -%d ,COST=%d\n", i,j,mst[i][j]);
+            }
+        }
+    }
+    
+    printf("The MST adj matrix");
+    for (int i = 0; i < num_vertex; i++)
+    {
+        printf("\n");
+        for (int j = 0; j < num_vertex; j++)
+        {
+            printf("%d ", mst[i][j]);
+        }
     }
 }
